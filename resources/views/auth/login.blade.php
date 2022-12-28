1256bee6ac778eff5d56bd0a8eb5e41985ae29c4
RaditiaSatriawan <raditiaoffice@gmail.com>
2022-12-28 12:42:23 +0700

    ditenv

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,29 +15,25 @@
        {{ csrf_field() }}
         <div class="txt_field">
           <input id="email" name="email" type="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" required autocomplete="email"  autofocus>
-         @error('email')
-         <span class="invalid-feedback" role="alert">
-             <strong>{{ $message }}</strong>
-          </span>
-        @enderror
-       
           <span></span>
           <label for="username">Email</label>
-
-
-        
-        </div>
+        </div>        
         <div class="txt_field">
           <input name="password" id="password" type="password" class="form-control @error('password') is-invalid @enderror"  required autocomplete="current-password" required>
-          @error('password')
-             <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-              </span>
-          @enderror
           <span></span>
           <label>Kata Sandi</label>
         </div>
         <input type="submit" value="Masuk">
+        @error('email')
+         <span class="invalid-feedback" role="alert">
+             <strong>{{ $message }}</strong>
+          </span>
+        @enderror
+        @error('password')
+             <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+              </span>
+        @enderror
         <div class="signup_link">
           Bukan Anggota? <a href="{{ route('register') }}">Daftar</a>
         </div>
